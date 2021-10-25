d1be374b25a23540588caa94c310dfdeca5758f4
WesleyOliveira01 <wesleyoliveira.dev@outlook.com>
2021-10-25 20:56:56 -0300

    v3

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+if('serviceWorker' in  navigator){
+    window.addEventListener("load",function(){
+        this.navigator.serviceWorker.register("service_work.js")
+        .then(function (registro){
+            console.log('service worker registrado com sucesso', registro)
+        })
+        .catch(function(erro){
+            console.log('service worker não registrou', erro)
+        })
+    })
+}
