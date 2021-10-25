704f99b3330e4a734fdbdf980d7af1c60d32741b
WesleyOliveira01 <wesleyoliveira.dev@outlook.com>
2021-10-25 20:23:07 -0300

    pwa v2

---
+++ js/service_work.js
@@ -0,0 +1,22 @@
+const cachename= 'cache-app-v1'
+
+const recursosChacheados = [
+    "styles.css",
+    "index.html",
+    "finalizar_compra.html",
+    "login.html",
+    "registro.html",
+    "visualizar.html",
+    "script.js",
+]
+
+self.addEventListener('install',function(event){
+    console.log('Service worker instalado')
+    event.waitUntil(caches.open(cachename).then(function(cache){
+        cache.addAll(recursosChacheados)
+    }))
+})
+
+self.addEventListener('activate', function(){
+    console.log("service worker ativado")
+})
