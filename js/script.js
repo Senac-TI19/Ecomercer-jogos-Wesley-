53ad715783971b1a0bedac695326044684b1261b
WesleyOliveira01 <wesleyoliveira.dev@outlook.com>
2021-10-27 21:38:52 -0300

    att

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 if('serviceWorker' in  navigator){
     window.addEventListener("load",function(){
-        this.navigator.serviceWorker.register("service_work.js")
+        this.navigator.serviceWorker.register("./serviceworker.js")
         .then(function (registro){
             console.log('service worker registrado com sucesso', registro)
         })
---
+++ serviceworker.js
@@ -0,0 +1,30 @@
+const cachename = 'cache-app-v1'
+
+const recursosChacheados = [
+    "./css/styles.css",
+    "./index.html",
+    "./finalizar_compra.html",
+    "./js/script.js",
+    "./produtos/bf.html",
+    "./produtos/Gow.html",
+    "./produtos/farcry.html",
+    "./img/BF.jpg",
+    "./img/gow.jpg",
+    "./img/farcry6.jpg",
+    "./img/pacman.jpg",
+    "./img/pacman.gif",
+    "./img/tijolos.jpg",
+    "./icon/favicon.ico",
+    "./fonts/Gameplay.ttf"
+]
+
+self.addEventListener('install',function(event){
+    console.log('Service worker instalado')
+    event.waitUntil(caches.open(cachename).then(function(cache){
+        cache.addAll(recursosChacheados)
+    }))
+})
+
+self.addEventListener('activate', function(){
+    console.log("service worker ativado")
+})
